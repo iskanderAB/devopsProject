f2bfe07a86efd9426b77607efd9cf5ea4bd08772
iskanderAB <iskander@keytchens.com>
2025-12-05 12:27:40 +0100

    fix: tets test test

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="container">
       <Link to="/" className="logo-font">
-        Conduitccccc
+        Conduit abdallah and iskander test 
       </Link>
       <span className="attribution">
         An interactive learning project from{" "}
